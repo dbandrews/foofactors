66ee2810fb9e441fdb3caab2b243344ad9677969
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-03-03 15:50:32 -0700

    Added in both funcs

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,13 @@
+#' Bind two factors
+#'
+#' @param a factor 1
+#' @param b factor 2
+#'
+#' @return combined factor 1 & 2
+#' @export
+#'
+#' @examples
+#' fbind(as.factor(c("a","b","c")), as.factor(c("d","e","f")))
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
